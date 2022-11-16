54b5018d6f2bc1a3c00d1df4b3c0791541baf45d
Preston <55302754+prestonmeek@users.noreply.github.com>
2022-11-16 14:11:06 -0500

    Got playing from folders working (library works!)

--- dfplayermini_example/dfplayermini_example.ino
+++ dfplayermini_example/dfplayermini_example.ino
@@ -1,94 +1,31 @@
-///              MP3 PLAYER PROJECT
-/// http://educ8s.tv/arduino-mp3-player/
-//////////////////////////////////////////
-
-// https://stackoverflow.com/questions/57175348/softwareserial-for-arduino-nano-33-iot
-// https://www.electronics-lab.com/project/mp3-player-using-arduino-dfplayer-mini/
-
 #include "SoftwareSerial.h"
-SoftwareSerial mySerial(11, 10);
+#include "DFRobotDFPlayerMini.h"
 
-# define Start_Byte 0x7E
-# define Version_Byte 0xFF
-# define Command_Length 0x06
-# define End_Byte 0xEF
-# define Acknowledge 0x00 //Returns info with command 0x41 [0x01: info, 0x00: no info]
+SoftwareSerial softwareSerial(11, 10);
 
-# define ACTIVATED LOW
+// Create the Player object
+DFRobotDFPlayerMini player;
 
+void setup() {
+  // Init USB serial port for debugging
+  Serial.begin(9600);
+  while (!Serial);
+  // Init serial port for DFPlayer Mini
+  softwareSerial.begin(9600);
 
-boolean isPlaying = false;
+  // Start communication with DFPlayer Mini
+  if (player.begin(softwareSerial)) {
+    Serial.println("OK");
 
+    // Set volume to maximum (0 to 30).
+    player.volume(30);
+    // Play the "0001.mp3" in the "mp3" folder on the SD card
+    player.playMp3Folder(1);
 
-
-void setup () {
-
-mySerial.begin (9600);
-delay(1000);
-playFirst();
-isPlaying = true;
-playNext();
-
-
+  } else {
+    Serial.println("Connecting to DFPlayer Mini failed!");
+  }
 }
 
-
-
-void loop () { 
-
-}
-
-void playFirst()
-{
-  execute_CMD(0x3F, 0, 0);
-  delay(500);
-  setVolume(20);
-  delay(500);
-  execute_CMD(0x11,0,1); 
-  delay(500);
-}
-
-void pause()
-{
-  execute_CMD(0x0E,0,0);
-  delay(500);
-}
-
-void play()
-{
-  execute_CMD(0x0D,0,1); 
-  delay(500);
-}
-
-void playNext()
-{
-  execute_CMD(0x01,0,1);
-  delay(500);
-}
-
-void playPrevious()
-{
-  execute_CMD(0x02,0,1);
-  delay(500);
-}
-
-void setVolume(int volume)
-{
-  execute_CMD(0x06, 0, volume); // Set the volume (0x00~0x30)
-  delay(2000);
-}
-
-void execute_CMD(byte CMD, byte Par1, byte Par2)
-// Excecute the command and parameters
-{
-// Calculate the checksum (2 bytes)
-word checksum = -(Version_Byte + Command_Length + CMD + Acknowledge + Par1 + Par2);
-// Build the command line
-byte Command_line[10] = { Start_Byte, Version_Byte, Command_Length, CMD, Acknowledge,
-Par1, Par2, highByte(checksum), lowByte(checksum), End_Byte};
-//Send the command line to the module
-for (byte k=0; k<10; k++)
-{
-mySerial.write( Command_line[k]);
-}
+void loop() {
 }
